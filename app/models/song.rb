2401959de9b1982df4d6872603f923bd46116613
Ivana <ivana_pile@yahoo.com>
2020-02-05 19:56:38 -0500

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,3 +1,11 @@
 class Song < ActiveRecord::Base
   # add associations here
+  belongs_to :artist
+  belongs_to :genre
+  has_many :notes
+
+  def genre_name=(genre_name)
+    genre = Genre.find_or_create_by(name: genre_name)
+    self.genre = genre
+  end
 end
